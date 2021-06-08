e675baaa991f3b250beb2c05b0fba9dc23f10c1a
murata0531 <coless115@gmail.com>
2021-06-08 11:47:48 +0900

    main

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -6,6 +6,7 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <stdio.h>
 
 // #@@range_begin(includes)
 #include "frame_buffer_config.hpp"
@@ -51,6 +52,13 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   for (char c = '!'; c <= '~'; ++c, ++i) {
     WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
   }
+  WriteString(*pixel_writer, 0, 66, "Hello, world!", {0, 0, 255});
   // #@@range_end(write_fonts)
+
+  // #@@range_begin(sprintf)
+  char buf[128];
+  sprintf(buf, "1 + 2 = %d", 1 + 2);
+  WriteString(*pixel_writer, 0, 82, buf, {0, 0, 0});
+  // #@@range_end(sprintf)
   while (1) __asm__("hlt");
 }
