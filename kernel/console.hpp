d51300ae70be0e8cbf8a0bade7ca2541d9e40b51
murata0531 <coless115@gmail.com>
2021-06-10 21:33:24 +0900

    console

--- kernel/console.hpp
+++ kernel/console.hpp
@@ -1,53 +1,20 @@
-/**
- * @file console.cpp
- *
- * コンソール描画のプログラムを集めたファイル．
- */
+#pragma once
 
-#include "console.hpp"
+#include "graphics.hpp"
 
-#include <string.h>
-#include "font.hpp"
+class Console {
+ public:
+  static const int kRows = 25, kColumns = 80;
 
-// #@@range_begin(constructor)
-Console::Console(PixelWriter& writer,
-    const PixelColor& fg_color, const PixelColor& bg_color)
-    : writer_{writer}, fg_color_{fg_color}, bg_color_{bg_color},
-      buffer_{}, cursor_row_{0}, cursor_column_{0} {
-}
-// #@@range_end(constructor)
+  Console(PixelWriter& writer,
+      const PixelColor& fg_color, const PixelColor& bg_color);
+  void PutString(const char* s);
 
-// #@@range_begin(put_string)
-void Console::PutString(const char* s) {
-  while (*s) {
-    if (*s == '\n') {
-      Newline();
-    } else if (cursor_column_ < kColumns - 1) {
-      WriteAscii(writer_, 8 * cursor_column_, 16 * cursor_row_, *s, fg_color_);
-      buffer_[cursor_row_][cursor_column_] = *s;
-      ++cursor_column_;
-    }
-    ++s;
-  }
-}
-// #@@range_end(put_string)
+ private:
+  void Newline();
 
-// #@@range_begin(newline)
-void Console::Newline() {
-  cursor_column_ = 0;
-  if (cursor_row_ < kRows - 1) {
-    ++cursor_row_;
-  } else {
-    for (int y = 0; y < 16 * kRows; ++y) {
-      for (int x = 0; x < 8 * kColumns; ++x) {
-        writer_.Write(x, y, bg_color_);
-      }
-    }
-    for (int row = 0; row < kRows - 1; ++row) {
-      memcpy(buffer_[row], buffer_[row + 1], kColumns + 1);
-      WriteString(writer_, 0, 16 * row, buffer_[row], fg_color_);
-    }
-    memset(buffer_[kRows - 1], 0, kColumns + 1);
-  }
-}
-// #@@range_end(newline)
+  PixelWriter& writer_;
+  const PixelColor fg_color_, bg_color_;
+  char buffer_[kRows][kColumns + 1];
+  int cursor_row_, cursor_column_;
+};
