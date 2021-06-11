637aab70ea94a7b6ac2690964c6cd8745e00620b
murata0531 <coless115@gmail.com>
2021-06-11 14:16:30 +0900

    newlib

--- kernel/newlib_support.c
+++ kernel/newlib_support.c
@@ -3,7 +3,20 @@
 #include <stdio.h>
 #include <errno.h>
 
+void _exit(void) {
+  while (1) __asm__("hlt");
+}
+
 caddr_t sbrk(int incr) {
   errno = ENOMEM;
   return (caddr_t)-1;
+}
+
+int getpid(void) {
+  return 1;
+}
+
+int kill(int pid, int sig) {
+  errno = EINVAL;
+  return -1;
 }
