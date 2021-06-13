9492425443d39522fe41ac553c59681bb332973c
murata0531 <coless115@gmail.com>
2021-06-14 01:36:09 +0900

    mouse

--- kernel/error.hpp
+++ kernel/error.hpp
@@ -26,6 +26,7 @@ class Error {
     kInvalidPhase,
     kUnknownXHCISpeedID,
     kNoWaiter,
+    kNoPCIMSI,
     kLastOfCode,  // この列挙子は常に最後に配置する
   };
 
@@ -51,6 +52,7 @@ class Error {
     "kInvalidPhase",
     "kUnknownXHCISpeedID",
     "kNoWaiter",
+    "kNoPCIMSI",
   };
   static_assert(Error::Code::kLastOfCode == code_names_.size());
 
@@ -85,10 +87,8 @@ class Error {
 
 #define MAKE_ERROR(code) Error((code), __FILE__, __LINE__)
 
-// #@@range_begin(with_error)
 template <class T>
 struct WithError {
   T value;
   Error error;
 };
-//
