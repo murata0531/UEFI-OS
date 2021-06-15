1f89c0dd974bbb8e075bd3eb9526d2bbeb9a21b7
murata0531 <coless115@gmail.com>
2021-06-15 12:27:58 +0900

    layer

--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -18,7 +18,6 @@
  * 現状では 1 つのウィンドウしか保持できない設計だが，
  * 将来的には複数のウィンドウを持ち得る。
  */
-// #@@range_begin(layer)
 class Layer {
  public:
   /** @brief 指定された ID を持つレイヤーを生成する。 */
@@ -36,22 +35,20 @@ class Layer {
   /** @brief レイヤーの位置情報を指定された相対座標へと更新する。再描画はしない。 */
   Layer& MoveRelative(Vector2D<int> pos_diff);
 
-  /** @brief writer に現在設定されているウィンドウの内容を描画する。 */
-  void DrawTo(PixelWriter& writer) const;
+  /** @brief 指定された描画先にウィンドウの内容を描画する。 */
+  void DrawTo(FrameBuffer& screen) const;
 
  private:
   unsigned int id_;
   Vector2D<int> pos_;
   std::shared_ptr<Window> window_;
 };
-// #@@range_end(layer)
 
 /** @brief LayerManager は複数のレイヤーを管理する。 */
-// #@@range_begin(layer_manager)
 class LayerManager {
  public:
   /** @brief Draw メソッドなどで描画する際の描画先を設定する。 */
-  void SetWriter(PixelWriter* writer);
+  void SetWriter(FrameBuffer* screen);
   /** @brief 新しいレイヤーを生成して参照を返す。
    *
    * 新しく生成されたレイヤーの実体は LayerManager 内部のコンテナで保持される。
@@ -76,14 +73,15 @@ class LayerManager {
   /** @brief レイヤーを非表示とする。 */
   void Hide(unsigned int id);
 
+  // #@@range_begin(layermgr_fields)
  private:
-  PixelWriter* writer_{nullptr};
+  FrameBuffer* screen_{nullptr};
   std::vector<std::unique_ptr<Layer>> layers_{};
   std::vector<Layer*> layer_stack_{};
   unsigned int latest_id_{0};
+  // #@@range_end(layermgr_fields)
 
   Layer* FindLayer(unsigned int id);
 };
 
 extern LayerManager* layer_manager;
-// #@@range_end(layer_manager)
