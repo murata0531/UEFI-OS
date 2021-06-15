0cea2c430649fbf041b3701fa9f9ad509c9b160a
murata0531 <coless115@gmail.com>
2021-06-15 11:20:43 +0900

    memory manager

--- kernel/newlib_support.c
+++ kernel/newlib_support.c
@@ -5,10 +5,20 @@ void _exit(void) {
   while (1) __asm__("hlt");
 }
 
+// #@@range_begin(sbrk)
+caddr_t program_break, program_break_end;
+
 caddr_t sbrk(int incr) {
-  errno = ENOMEM;
-  return (caddr_t)-1;
+  if (program_break == 0 || program_break + incr >= program_break_end) {
+    errno = ENOMEM;
+    return (caddr_t)-1;
+  }
+
+  caddr_t prev_break = program_break;
+  program_break += incr;
+  return prev_break;
 }
+// #@@range_end(sbrk)
 
 int getpid(void) {
   return 1;
