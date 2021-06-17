2d77e013c29f92d5c6338c07bb7328f62118208f
murata0531 <coless115@gmail.com>
2021-06-17 21:11:01 +0900

    layer

--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -31,6 +31,10 @@ class Layer {
   std::shared_ptr<Window> GetWindow() const;
   /** @brief レイヤーの原点座標を取得する。 */
   Vector2D<int> GetPosition() const;
+  /** @brief true でレイヤーがドラッグ移動可能となる。 */
+  Layer& SetDraggable(bool draggable);
+  /** @brief レイヤーがドラッグ移動可能なら true を返す。 */
+  bool IsDraggable() const;
 
   /** @brief レイヤーの位置情報を指定された絶対座標へと更新する。再描画はしない。 */
   Layer& Move(Vector2D<int> pos);
@@ -40,10 +44,13 @@ class Layer {
   /** @brief 指定された描画先にウィンドウの内容を描画する。 */
   void DrawTo(FrameBuffer& screen, const Rectangle<int>& area) const;
 
+  // #@@range_begin(fields)
  private:
   unsigned int id_;
-  Vector2D<int> pos_;
-  std::shared_ptr<Window> window_;
+  Vector2D<int> pos_{};
+  std::shared_ptr<Window> window_{};
+  bool draggable_{false};
+  // #@@range_end(fields)
 };
 
 /** @brief LayerManager は複数のレイヤーを管理する。 */
