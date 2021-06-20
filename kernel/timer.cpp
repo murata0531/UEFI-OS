aa1b8272a8ea32d6d84bbee0c9abd1438a2b6a7a
murata0531 <coless115@gmail.com>
2021-06-20 16:21:48 +0900

    timer

--- kernel/timer.cpp
+++ kernel/timer.cpp
@@ -10,15 +10,13 @@ namespace {
   volatile uint32_t& divide_config = *reinterpret_cast<uint32_t*>(0xfee003e0);
 }
 
-// #@@range_begin(init_timer)
-void InitializeLAPICTimer() {
-  timer_manager = new TimerManager;
+void InitializeLAPICTimer(std::deque<Message>& msg_queue) {
+  timer_manager = new TimerManager{msg_queue};
 
   divide_config = 0b1011; // divide 1:1
   lvt_timer = (0b010 << 16) | InterruptVector::kLAPICTimer; // not-masked, periodic
   initial_count = 0x1000000u;
 }
-// #@@range_end(init_timer)
 
 void StartLAPICTimer() {
   initial_count = kCountMax;
@@ -32,14 +30,42 @@ void StopLAPICTimer() {
   initial_count = 0;
 }
 
+Timer::Timer(unsigned long timeout, int value)
+    : timeout_{timeout}, value_{value} {
+}
+
+// #@@range_begin(timermgr_addtimer)
+TimerManager::TimerManager(std::deque<Message>& msg_queue)
+    : msg_queue_{msg_queue} {
+  timers_.push(Timer{std::numeric_limits<unsigned long>::max(), -1});
+}
+
+void TimerManager::AddTimer(const Timer& timer) {
+  timers_.push(timer);
+}
+// #@@range_end(timermgr_addtimer)
+
 // #@@range_begin(timermgr_tick)
 void TimerManager::Tick() {
   ++tick_;
+  while (true) {
+    const auto& t = timers_.top();
+    if (t.Timeout() > tick_) {
+      break;
+    }
+
+    Message m{Message::kTimerTimeout};
+    m.arg.timer.timeout = t.Timeout();
+    m.arg.timer.value = t.Value();
+    msg_queue_.push_back(m);
+
+    timers_.pop();
+  }
 }
+// #@@range_end(timermgr_tick)
 
 TimerManager* timer_manager;
 
 void LAPICTimerOnInterrupt() {
   timer_manager->Tick();
 }
-// #@@range_end(timermgr_tick)
