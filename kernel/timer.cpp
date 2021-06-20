e3c727d126b266eb65d5530136355fb9c7accb8e
murata0531 <coless115@gmail.com>
2021-06-20 15:38:32 +0900

    timer

--- kernel/timer.cpp
+++ kernel/timer.cpp
@@ -1,5 +1,7 @@
 #include "timer.hpp"
 
+#include "interrupt.hpp"
+
 namespace {
   const uint32_t kCountMax = 0xffffffffu;
   volatile uint32_t& lvt_timer = *reinterpret_cast<uint32_t*>(0xfee00320);
@@ -8,10 +10,13 @@ namespace {
   volatile uint32_t& divide_config = *reinterpret_cast<uint32_t*>(0xfee003e0);
 }
 
+// #@@range_begin(init_timer)
 void InitializeLAPICTimer() {
   divide_config = 0b1011; // divide 1:1
-  lvt_timer = (0b001 << 16) | 32; // masked, one-shot
+  lvt_timer = (0b010 << 16) | InterruptVector::kLAPICTimer; // not-masked, periodic
+  initial_count = kCountMax;
 }
+// #@@range_end(init_timer)
 
 void StartLAPICTimer() {
   initial_count = kCountMax;
