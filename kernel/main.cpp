b269f336ad8c6dbf5b70cbd1ecb4503f7926b25b
murata0531 <coless115@gmail.com>
2021-06-20 15:41:35 +0900

    main

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -30,6 +30,7 @@
 #include "window.hpp"
 #include "layer.hpp"
 #include "message.hpp"
+#include "timer.hpp"
 
 int printk(const char* format, ...) {
   va_list ap;
@@ -64,7 +65,6 @@ std::deque<Message>* main_queue;
 
 alignas(16) uint8_t kernel_main_stack[1024 * 1024];
 
-// #@@range_begin(main_function)
 extern "C" void KernelMainNewStack(
     const FrameBufferConfig& frame_buffer_config_ref,
     const MemoryMap& memory_map_ref) {
@@ -89,7 +89,10 @@ extern "C" void KernelMainNewStack(
   InitializeMainWindow();
   InitializeMouse();
   layer_manager->Draw({{0, 0}, ScreenSize()});
-// #@@range_end(main_function)
+
+  // #@@range_begin(call_init_timer)
+  InitializeLAPICTimer();
+  // #@@range_end(call_init_timer)
 
   char str[128];
   unsigned int count = 0;
@@ -111,10 +114,15 @@ extern "C" void KernelMainNewStack(
     main_queue->pop_front();
     __asm__("sti");
 
+    // #@@range_begin(process_event)
     switch (msg.type) {
     case Message::kInterruptXHCI:
       usb::xhci::ProcessEvents();
       break;
+    case Message::kInterruptLAPICTimer:
+      printk("Timer interrupt\n");
+      break;
+    // #@@range_end(process_event)
     default:
       Log(kError, "Unknown message type: %d\n", msg.type);
     }
