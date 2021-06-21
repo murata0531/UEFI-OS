0088143e412f4ee951fbf185432c5e02922cc8a4
murata0531 <coless115@gmail.com>
2021-06-21 15:24:50 +0900

    main

--- kernel/asmfunc.asm
+++ kernel/asmfunc.asm
@@ -37,7 +37,6 @@ LoadIDT:
     pop rbp
     ret
 
-; #@@range_begin(load_gdt)
 global LoadGDT  ; void LoadGDT(uint16_t limit, uint64_t offset);
 LoadGDT:
     push rbp
@@ -49,9 +48,7 @@ LoadGDT:
     mov rsp, rbp
     pop rbp
     ret
-; #@@range_end(load_gdt)
 
-; #@@range_begin(set_cs)
 global SetCSSS  ; void SetCSSS(uint16_t cs, uint16_t ss);
 SetCSSS:
     push rbp
@@ -65,9 +62,7 @@ SetCSSS:
     mov rsp, rbp
     pop rbp
     ret
-; #@@range_end(set_cs)
 
-; #@@range_begin(set_dsall)
 global SetDSAll  ; void SetDSAll(uint16_t value);
 SetDSAll:
     mov ds, di
@@ -75,16 +70,19 @@ SetDSAll:
     mov fs, di
     mov gs, di
     ret
-; #@@range_end(set_dsall)
 
-; #@@range_begin(set_cr3)
 global SetCR3  ; void SetCR3(uint64_t value);
 SetCR3:
     mov cr3, rdi
     ret
-; #@@range_end(set_cr3)
 
-; #@@range_begin(set_main_stack)
+; #@@range_begin(get_cr3)
+global GetCR3  ; uint64_t GetCR3();
+GetCR3:
+    mov rax, cr3
+    ret
+; #@@range_end(get_cr3)
+
 extern kernel_main_stack
 extern KernelMainNewStack
 
@@ -95,4 +93,81 @@ KernelMain:
 .fin:
     hlt
     jmp .fin
-; #@@range_end(set_main_stack)
+
+; #@@range_begin(switch_context)
+global SwitchContext
+SwitchContext:  ; void SwitchContext(void* next_ctx, void* current_ctx);
+    mov [rsi + 0x40], rax
+    mov [rsi + 0x48], rbx
+    mov [rsi + 0x50], rcx
+    mov [rsi + 0x58], rdx
+    mov [rsi + 0x60], rdi
+    mov [rsi + 0x68], rsi
+
+    lea rax, [rsp + 8]
+    mov [rsi + 0x70], rax  ; RSP
+    mov [rsi + 0x78], rbp
+
+    mov [rsi + 0x80], r8
+    mov [rsi + 0x88], r9
+    mov [rsi + 0x90], r10
+    mov [rsi + 0x98], r11
+    mov [rsi + 0xa0], r12
+    mov [rsi + 0xa8], r13
+    mov [rsi + 0xb0], r14
+    mov [rsi + 0xb8], r15
+
+    mov rax, cr3
+    mov [rsi + 0x00], rax  ; CR3
+    mov rax, [rsp]
+    mov [rsi + 0x08], rax  ; RIP
+    pushfq
+    pop qword [rsi + 0x10] ; RFLAGS
+
+    mov ax, cs
+    mov [rsi + 0x20], rax
+    mov bx, ss
+    mov [rsi + 0x28], rbx
+    mov cx, fs
+    mov [rsi + 0x30], rcx
+    mov dx, gs
+    mov [rsi + 0x38], rdx
+
+    fxsave [rsi + 0xc0]
+
+    ; iret 用のスタックフレーム
+    push qword [rdi + 0x28] ; SS
+    push qword [rdi + 0x70] ; RSP
+    push qword [rdi + 0x10] ; RFLAGS
+    push qword [rdi + 0x20] ; CS
+    push qword [rdi + 0x08] ; RIP
+
+    ; コンテキストの復帰
+    fxrstor [rdi + 0xc0]
+
+    mov rax, [rdi + 0x00]
+    mov cr3, rax
+    mov rax, [rdi + 0x30]
+    mov fs, ax
+    mov rax, [rdi + 0x38]
+    mov gs, ax
+
+    mov rax, [rdi + 0x40]
+    mov rbx, [rdi + 0x48]
+    mov rcx, [rdi + 0x50]
+    mov rdx, [rdi + 0x58]
+    mov rsi, [rdi + 0x68]
+    mov rbp, [rdi + 0x78]
+    mov r8,  [rdi + 0x80]
+    mov r9,  [rdi + 0x88]
+    mov r10, [rdi + 0x90]
+    mov r11, [rdi + 0x98]
+    mov r12, [rdi + 0xa0]
+    mov r13, [rdi + 0xa8]
+    mov r14, [rdi + 0xb0]
+    mov r15, [rdi + 0xb8]
+
+    mov rdi, [rdi + 0x60]
+
+    o64 iret
+; #@@range_end(switch_context)
