84b5bd78888c660973b11bff9311904a6d15d1f2
murata0531 <coless115@gmail.com>
2021-06-21 15:22:36 +0900

    main

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -8,10 +8,11 @@
 #include <cstddef>
 #include <cstdio>
 
-#include <numeric>
-#include <vector>
+#include <array>
 #include <deque>
 #include <limits>
+#include <numeric>
+#include <vector>
 
 #include "frame_buffer_config.hpp"
 #include "memory_map.hpp"
@@ -85,15 +86,12 @@ void InitializeTextWindow() {
 
 int text_window_index;
 
-// #@@range_begin(draw_cursor)
 void DrawTextCursor(bool visible) {
   const auto color = visible ? ToColor(0) : ToColor(0xffffff);
   const auto pos = Vector2D<int>{8 + 8*text_window_index, 24 + 5};
   FillRectangle(*text_window->Writer(), pos, {7, 15}, color);
 }
-// #@@range_end(draw_cursor)
 
-// #@@range_begin(input_text)
 void InputTextWindow(char c) {
   if (c == 0) {
     return;
@@ -116,7 +114,53 @@ void InputTextWindow(char c) {
 
   layer_manager->Draw(text_window_layer_id);
 }
-// #@@range_end(input_text)
+
+// #@@range_begin(taskb_window)
+std::shared_ptr<Window> task_b_window;
+unsigned int task_b_window_layer_id;
+void InitializeTaskBWindow() {
+  task_b_window = std::make_shared<Window>(
+      160, 52, screen_config.pixel_format);
+  DrawWindow(*task_b_window->Writer(), "TaskB Window");
+
+  task_b_window_layer_id = layer_manager->NewLayer()
+    .SetWindow(task_b_window)
+    .SetDraggable(true)
+    .Move({100, 100})
+    .ID();
+
+  layer_manager->UpDown(task_b_window_layer_id, std::numeric_limits<int>::max());
+}
+// #@@range_end(taskb_window)
+
+// #@@range_begin(task_context)
+struct TaskContext {
+  uint64_t cr3, rip, rflags, reserved1; // offset 0x00
+  uint64_t cs, ss, fs, gs; // offset 0x20
+  uint64_t rax, rbx, rcx, rdx, rdi, rsi, rsp, rbp; // offset 0x40
+  uint64_t r8, r9, r10, r11, r12, r13, r14, r15; // offset 0x80
+  std::array<uint8_t, 512> fxsave_area; // offset 0xc0
+} __attribute__((packed));
+
+alignas(16) TaskContext task_b_ctx, task_a_ctx;
+// #@@range_end(task_context)
+
+// #@@range_begin(taskb_func)
+void TaskB(int task_id, int data) {
+  printk("TaskB: task_id=%d, data=%d\n", task_id, data);
+  char str[128];
+  int count = 0;
+  while (true) {
+    ++count;
+    sprintf(str, "%010d", count);
+    FillRectangle(*task_b_window->Writer(), {24, 28}, {8 * 10, 16}, {0xc6, 0xc6, 0xc6});
+    WriteString(*task_b_window->Writer(), {24, 28}, str, {0, 0, 0});
+    layer_manager->Draw(task_b_window_layer_id);
+
+    SwitchContext(&task_a_ctx, &task_b_ctx);
+  }
+}
+// #@@range_end(taskb_func)
 
 std::deque<Message>* main_queue;
 
@@ -146,6 +190,7 @@ extern "C" void KernelMainNewStack(
   InitializeLayer();
   InitializeMainWindow();
   InitializeTextWindow();
+  InitializeTaskBWindow();
   InitializeMouse();
   layer_manager->Draw({{0, 0}, ScreenSize()});
 
@@ -154,14 +199,31 @@ extern "C" void KernelMainNewStack(
 
   InitializeKeyboard(*main_queue);
 
-  // #@@range_begin(add_timer)
   const int kTextboxCursorTimer = 1;
   const int kTimer05Sec = static_cast<int>(kTimerFreq * 0.5);
   __asm__("cli");
   timer_manager->AddTimer(Timer{kTimer05Sec, kTextboxCursorTimer});
   __asm__("sti");
   bool textbox_cursor_visible = false;
-  // #@@range_end(add_timer)
+
+  // #@@range_begin(init_taskb)
+  std::vector<uint64_t> task_b_stack(1024);
+  uint64_t task_b_stack_end = reinterpret_cast<uint64_t>(&task_b_stack[1024]);
+
+  memset(&task_b_ctx, 0, sizeof(task_b_ctx));
+  task_b_ctx.rip = reinterpret_cast<uint64_t>(TaskB);
+  task_b_ctx.rdi = 1;
+  task_b_ctx.rsi = 42;
+
+  task_b_ctx.cr3 = GetCR3();
+  task_b_ctx.rflags = 0x202;
+  task_b_ctx.cs = kKernelCS;
+  task_b_ctx.ss = kKernelSS;
+  task_b_ctx.rsp = (task_b_stack_end & ~0xflu) - 8;
+
+  // MXCSR のすべての例外をマスクする
+  *reinterpret_cast<uint32_t*>(&task_b_ctx.fxsave_area[24]) = 0x1f80;
+  // #@@range_end(init_taskb)
 
   char str[128];
 
@@ -175,11 +237,14 @@ extern "C" void KernelMainNewStack(
     WriteString(*main_window->Writer(), {24, 28}, str, {0, 0, 0});
     layer_manager->Draw(main_window_layer_id);
 
+    // #@@range_begin(switch_to_taskb)
     __asm__("cli");
     if (main_queue->size() == 0) {
-      __asm__("sti\n\thlt");
+      __asm__("sti");
+      SwitchContext(&task_b_ctx, &task_a_ctx);
       continue;
     }
+    // #@@range_end(switch_to_taskb)
 
     Message msg = main_queue->front();
     main_queue->pop_front();
@@ -189,7 +254,6 @@ extern "C" void KernelMainNewStack(
     case Message::kInterruptXHCI:
       usb::xhci::ProcessEvents();
       break;
-    // #@@range_begin(timer_event)
     case Message::kTimerTimeout:
       if (msg.arg.timer.value == kTextboxCursorTimer) {
         __asm__("cli");
@@ -201,7 +265,6 @@ extern "C" void KernelMainNewStack(
         layer_manager->Draw(text_window_layer_id);
       }
       break;
-    // #@@range_end(timer_event)
     case Message::kKeyPush:
       InputTextWindow(msg.arg.keyboard.ascii);
       break;
