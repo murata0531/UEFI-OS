caef7234d2a5837463aaef648dfeb03b85dbcd81
murata0531 <coless115@gmail.com>
2021-06-22 09:38:26 +0900

    task

--- kernel/task.cpp
+++ kernel/task.cpp
@@ -4,12 +4,9 @@
 #include "segment.hpp"
 #include "timer.hpp"
 
-// #@@range_begin(task_ctor)
 Task::Task(uint64_t id) : id_{id} {
 }
-// #@@range_end(task_ctor)
 
-// #@@range_begin(task_initctx)
 Task& Task::InitContext(TaskFunc* f, int64_t data) {
   const size_t stack_size = kDefaultStackBytes / sizeof(stack_[0]);
   stack_.resize(stack_size);
@@ -31,45 +28,105 @@ Task& Task::InitContext(TaskFunc* f, int64_t data) {
 
   return *this;
 }
-// #@@range_end(task_initctx)
 
-// #@@range_begin(task_context)
 TaskContext& Task::Context() {
   return context_;
 }
-// #@@range_end(task_context)
+
+// #@@range_begin(task_methods)
+uint64_t Task::ID() const {
+  return id_;
+}
+
+Task& Task::Sleep() {
+  task_manager->Sleep(this);
+  return *this;
+}
+
+Task& Task::Wakeup() {
+  task_manager->Wakeup(this);
+  return *this;
+}
+// #@@range_end(task_methods)
 
 // #@@range_begin(taskmgr_ctor)
 TaskManager::TaskManager() {
-  NewTask();
+  running_.push_back(&NewTask());
 }
 // #@@range_end(taskmgr_ctor)
 
-// #@@range_begin(taskmgr_newtask)
 Task& TaskManager::NewTask() {
   ++latest_id_;
   return *tasks_.emplace_back(new Task{latest_id_});
 }
-// #@@range_end(taskmgr_newtask)
 
-// #@@range_begin(taskmgr_switchtask)
-void TaskManager::SwitchTask() {
-  size_t next_task_index = current_task_index_ + 1;
-  if (next_task_index >= tasks_.size()) {
-    next_task_index = 0;
+// #@@range_begin(taskmgr_swtask)
+void TaskManager::SwitchTask(bool current_sleep) {
+  Task* current_task = running_.front();
+  running_.pop_front();
+  if (!current_sleep) {
+    running_.push_back(current_task);
+  }
+  Task* next_task = running_.front();
+
+  SwitchContext(&next_task->Context(), &current_task->Context());
+}
+// #@@range_end(taskmgr_swtask)
+
+// #@@range_begin(taskmgr_sleep)
+void TaskManager::Sleep(Task* task) {
+  auto it = std::find(running_.begin(), running_.end(), task);
+
+  if (it == running_.begin()) {
+    SwitchTask(true);
+    return;
   }
 
-  Task& current_task = *tasks_[current_task_index_];
-  Task& next_task = *tasks_[next_task_index];
-  current_task_index_ = next_task_index;
+  if (it == running_.end()) {
+    return;
+  }
 
-  SwitchContext(&next_task.Context(), &current_task.Context());
+  running_.erase(it);
 }
-// #@@range_end(taskmgr_switchtask)
+// #@@range_end(taskmgr_sleep)
+
+// #@@range_begin(taskmgr_sleep_id)
+Error TaskManager::Sleep(uint64_t id) {
+  auto it = std::find_if(tasks_.begin(), tasks_.end(),
+                         [id](const auto& t){ return t->ID() == id; });
+  if (it == tasks_.end()) {
+    return MAKE_ERROR(Error::kNoSuchTask);
+  }
+
+  Sleep(it->get());
+  return MAKE_ERROR(Error::kSuccess);
+}
+// #@@range_end(taskmgr_sleep_id)
+
+// #@@range_begin(taskmgr_wakeup)
+void TaskManager::Wakeup(Task* task) {
+  auto it = std::find(running_.begin(), running_.end(), task);
+  if (it == running_.end()) {
+    running_.push_back(task);
+  }
+}
+// #@@range_end(taskmgr_wakeup)
+
+// #@@range_begin(taskmgr_wakeup_id)
+Error TaskManager::Wakeup(uint64_t id) {
+  auto it = std::find_if(tasks_.begin(), tasks_.end(),
+                         [id](const auto& t){ return t->ID() == id; });
+  if (it == tasks_.end()) {
+    return MAKE_ERROR(Error::kNoSuchTask);
+  }
+
+  Wakeup(it->get());
+  return MAKE_ERROR(Error::kSuccess);
+}
+// #@@range_end(taskmgr_wakeup_id)
 
 TaskManager* task_manager;
 
-// #@@range_begin(inittask)
 void InitializeTask() {
   task_manager = new TaskManager;
 
@@ -78,4 +135,3 @@ void InitializeTask() {
       Timer{timer_manager->CurrentTick() + kTaskTimerPeriod, kTaskTimerValue});
   __asm__("sti");
 }
-// #@@range_end(inittask)
--- kernel/task.hpp
+++ kernel/task.hpp
@@ -9,7 +9,10 @@
 #include <array>
 #include <cstddef>
 #include <cstdint>
+#include <deque>
 #include <vector>
+
+#include "error.hpp"
 
 struct TaskContext {
   uint64_t cr3, rip, rflags, reserved1; // offset 0x00
@@ -19,7 +22,6 @@ struct TaskContext {
   std::array<uint8_t, 512> fxsave_area; // offset 0xc0
 } __attribute__((packed));
 
-// #@@range_begin(task)
 using TaskFunc = void (uint64_t, int64_t);
 
 class Task {
@@ -29,28 +31,35 @@ class Task {
   Task(uint64_t id);
   Task& InitContext(TaskFunc* f, int64_t data);
   TaskContext& Context();
+  uint64_t ID() const;
+  Task& Sleep();
+  Task& Wakeup();
 
  private:
   uint64_t id_;
   std::vector<uint64_t> stack_;
   alignas(16) TaskContext context_;
 };
-// #@@range_end(task)
 
 // #@@range_begin(taskmgr)
 class TaskManager {
  public:
   TaskManager();
   Task& NewTask();
-  void SwitchTask();
+  void SwitchTask(bool current_sleep = false);
+
+  void Sleep(Task* task);
+  Error Sleep(uint64_t id);
+  void Wakeup(Task* task);
+  Error Wakeup(uint64_t id);
 
  private:
   std::vector<std::unique_ptr<Task>> tasks_{};
   uint64_t latest_id_{0};
-  size_t current_task_index_{0};
+  std::deque<Task*> running_{};
 };
+// #@@range_end(taskmgr)
 
 extern TaskManager* task_manager;
-// #@@range_end(taskmgr)
 
 void InitializeTask();
