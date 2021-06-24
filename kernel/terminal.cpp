f44158b6ccced2a8c4c92a130b3d43d27c815802
murata0531 <coless115@gmail.com>
2021-06-24 14:23:07 +0900

    terminal

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -4,6 +4,7 @@
 
 #include "font.hpp"
 #include "layer.hpp"
+#include "pci.hpp"
 
 Terminal::Terminal() {
   window_ = std::make_shared<ToplevelWindow>(
@@ -99,7 +100,6 @@ void Terminal::ExecuteLine() {
     ++first_arg;
   }
 
-  // #@@range_begin(clear_command)
   if (strcmp(command, "echo") == 0) {
     if (first_arg) {
       Print(first_arg);
@@ -109,8 +109,19 @@ void Terminal::ExecuteLine() {
     FillRectangle(*window_->InnerWriter(),
                   {4, 4}, {8*kColumns, 16*kRows}, {0, 0, 0});
     cursor_.y = 0;
+  // #@@range_begin(lspci_command)
+  } else if (strcmp(command, "lspci") == 0) {
+    char s[64];
+    for (int i = 0; i < pci::num_device; ++i) {
+      const auto& dev = pci::devices[i];
+      auto vendor_id = pci::ReadVendorId(dev.bus, dev.device, dev.function);
+      sprintf(s, "%02x:%02x.%d vend=%04x head=%02x class=%02x.%02x.%02x\n",
+          dev.bus, dev.device, dev.function, vendor_id, dev.header_type,
+          dev.class_code.base, dev.class_code.sub, dev.class_code.interface);
+      Print(s);
+    }
   } else if (command[0] != 0) {
-  // #@@range_end(clear_command)
+  // #@@range_end(lspci_command)
     Print("no such command: ");
     Print(command);
     Print("\n");
