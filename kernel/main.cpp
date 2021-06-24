84ea5942525ac4fab226f91484efb4e99232d79c
murata0531 <coless115@gmail.com>
2021-06-24 15:47:40 +0900

    main

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -116,10 +116,13 @@ void InputTextWindow(char c) {
 
 alignas(16) uint8_t kernel_main_stack[1024 * 1024];
 
+// #@@range_begin(volume_arg)
 extern "C" void KernelMainNewStack(
     const FrameBufferConfig& frame_buffer_config_ref,
     const MemoryMap& memory_map_ref,
-    const acpi::RSDP& acpi_table) {
+    const acpi::RSDP& acpi_table,
+    void* volume_image) {
+// #@@range_end(volume_arg)
   MemoryMap memory_map{memory_map_ref};
 
   InitializeGraphics(frame_buffer_config_ref);
@@ -158,6 +161,24 @@ extern "C" void KernelMainNewStack(
   usb::xhci::Initialize();
   InitializeKeyboard();
   InitializeMouse();
+
+  // #@@range_begin(dump_volume)
+  uint8_t* p = reinterpret_cast<uint8_t*>(volume_image);
+  printk("Volume Image:\n");
+  for (int i = 0; i < 16; ++i) {
+    printk("%04x:", i * 16);
+    for (int j = 0; j < 8; ++j) {
+      printk(" %02x", *p);
+      ++p;
+    }
+    printk(" ");
+    for (int j = 0; j < 8; ++j) {
+      printk(" %02x", *p);
+      ++p;
+    }
+    printk("\n");
+  }
+  // #@@range_end(dump_volume)
 
   char str[128];
 
