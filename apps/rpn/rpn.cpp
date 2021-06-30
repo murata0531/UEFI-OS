4fabdf90874ad62c6979aaa2de22bcb208274947
murata0531 <coless115@gmail.com>
2021-06-30 21:17:48 +0900

    rpn

--- apps/rpn/rpn.cpp
+++ apps/rpn/rpn.cpp
@@ -1,5 +1,8 @@
 #include <cstring>
 #include <cstdlib>
+#include <cstdint>
+
+#include "../../kernel/logger.hpp"
 
 int stack_ptr;
 long stack[100];
@@ -15,6 +18,9 @@ void Push(long value) {
   stack[stack_ptr] = value;
 }
 
+// #@@range_begin(call_syscall)
+extern "C" int64_t SyscallLogString(LogLevel, const char*);
+
 extern "C" int main(int argc, char** argv) {
   stack_ptr = -1;
 
@@ -23,20 +29,23 @@ extern "C" int main(int argc, char** argv) {
       long b = Pop();
       long a = Pop();
       Push(a + b);
+      SyscallLogString(kWarn, "+");
     } else if (strcmp(argv[i], "-") == 0) {
       long b = Pop();
       long a = Pop();
       Push(a - b);
+      SyscallLogString(kWarn, "-");
     } else {
       long a = atol(argv[i]);
       Push(a);
+      SyscallLogString(kWarn, "#");
     }
   }
-  // #@@range_begin(rpn_main)
   if (stack_ptr < 0) {
     return 0;
   }
+  SyscallLogString(kWarn, "\nhello, this is rpn\n");
   while (1);
   //return static_cast<int>(Pop());
-  // #@@range_end(rpn_main)
 }
+// #@@range_end(call_syscall)
