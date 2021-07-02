2b87c935625e82dda80698993e55fe46c31ab547
murata0531 <coless115@gmail.com>
2021-07-03 00:00:40 +0900

    system call

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -9,6 +9,7 @@
 #include "logger.hpp"
 #include "task.hpp"
 #include "terminal.hpp"
+#include "font.hpp"
 
 namespace syscall {
   struct Result {
@@ -50,14 +51,53 @@ SYSCALL(PutString) {
   return { 0, EBADF };
 }
 
-// #@@range_begin(syscall_exit)
 SYSCALL(Exit) {
   __asm__("cli");
   auto& task = task_manager->CurrentTask();
   __asm__("sti");
   return { task.OSStackPointer(), static_cast<int>(arg1) };
 }
-// #@@range_end(syscall_exit)
+
+SYSCALL(OpenWindow) {
+  const int w = arg1, h = arg2, x = arg3, y = arg4;
+  const auto title = reinterpret_cast<const char*>(arg5);
+  const auto win = std::make_shared<ToplevelWindow>(
+      w, h, screen_config.pixel_format, title);
+
+  __asm__("cli");
+  const auto layer_id = layer_manager->NewLayer()
+    .SetWindow(win)
+    .SetDraggable(true)
+    .Move({x, y})
+    .ID();
+  active_layer->Activate(layer_id);
+  __asm__("sti");
+
+  return { layer_id, 0 };
+}
+
+// #@@range_begin(write_string)
+SYSCALL(WinWriteString) {
+  const unsigned int layer_id = arg1;
+  const int x = arg2, y = arg3;
+  const uint32_t color = arg4;
+  const auto s = reinterpret_cast<const char*>(arg5);
+
+  __asm__("cli");
+  auto layer = layer_manager->FindLayer(layer_id);
+  __asm__("sti");
+  if (layer == nullptr) {
+    return { 0, EBADF };
+  }
+
+  WriteString(*layer->GetWindow()->Writer(), {x, y}, s, ToColor(color));
+  __asm__("cli");
+  layer_manager->Draw(layer_id);
+  __asm__("sti");
+
+  return { 0, 0 };
+}
+// #@@range_end(write_string)
 
 #undef SYSCALL
 
@@ -65,10 +105,12 @@ SYSCALL(Exit) {
 
 using SyscallFuncType = syscall::Result (uint64_t, uint64_t, uint64_t,
                                          uint64_t, uint64_t, uint64_t);
-extern "C" std::array<SyscallFuncType*, 3> syscall_table{
+extern "C" std::array<SyscallFuncType*, 5> syscall_table{
   /* 0x00 */ syscall::LogString,
   /* 0x01 */ syscall::PutString,
   /* 0x02 */ syscall::Exit,
+  /* 0x03 */ syscall::OpenWindow,
+  /* 0x04 */ syscall::WinWriteString,
 };
 
 void InitializeSyscall() {
