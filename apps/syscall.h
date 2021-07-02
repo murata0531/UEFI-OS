fc719daa7adc4bd4e9992f02db23c5d67bec9c17
murata0531 <coless115@gmail.com>
2021-07-02 23:33:17 +0900

    system call

--- apps/syscall.h
+++ apps/syscall.h
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <cstdint>
 #include "../kernel/logger.hpp"
 
@@ -11,5 +12,7 @@ struct SyscallResult {
 SyscallResult SyscallLogString(LogLevel level, const char* message);
 SyscallResult SyscallPutString(int fd, const char* s, size_t len);
 void SyscallExit(int exit_code);
+SyscallResult SyscallOpenWindow(int w, int h, int x, int y, const char* title);
+SyscallResult SyscallWinWriteString(unsigned int layer_id, int x, int y, uint32_t color, const char* s);
 
 } // extern "C"
