b0e2fdcfe2cca7a77f81783e1e9847b2a84ba89c
murata0531 <coless115@gmail.com>
2021-07-13 10:00:48 +0900

    アライメントされたメモリ領域を確保するposix_memalign

--- apps/newlib_support.c
+++ apps/newlib_support.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <signal.h>
 
 #include "syscall.h"
@@ -35,7 +36,6 @@ off_t lseek(int fd, off_t offset, int whence) {
   return -1;
 }
 
-// #@@range_begin(newlib_open)
 int open(const char* path, int flags) {
   struct SyscallResult res = SyscallOpenFile(path, flags);
   if (res.error == 0) {
@@ -44,9 +44,19 @@ int open(const char* path, int flags) {
   errno = res.error;
   return -1;
 }
-// #@@range_end(newlib_open)
 
-// #@@range_begin(newlib_read)
+// #@@range_begin(posix_memalign)
+int posix_memalign(void** memptr, size_t alignment, size_t size) {
+  void* p = malloc(size + alignment - 1);
+  if (!p) {
+    return ENOMEM;
+  }
+  uintptr_t addr = (uintptr_t)p;
+  *memptr = (void*)((addr + alignment - 1) & ~(uintptr_t)(alignment - 1));
+  return 0;
+}
+// #@@range_end(posix_memalign)
+
 ssize_t read(int fd, void* buf, size_t count) {
   struct SyscallResult res = SyscallReadFile(fd, buf, count);
   if (res.error == 0) {
@@ -55,9 +65,7 @@ ssize_t read(int fd, void* buf, size_t count) {
   errno = res.error;
   return -1;
 }
-// #@@range_end(newlib_read)
 
-// #@@range_begin(newlib_sbrk)
 caddr_t sbrk(int incr) {
   static uint8_t heap[4096];
   static int i = 0;
@@ -65,7 +73,6 @@ caddr_t sbrk(int incr) {
   i += incr;
   return (caddr_t)&heap[prev];
 }
-// #@@range_end(newlib_sbrk)
 
 ssize_t write(int fd, const void* buf, size_t count) {
   struct SyscallResult res = SyscallPutString(fd, buf, count);
