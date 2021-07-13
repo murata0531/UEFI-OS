a2aaa359c701beb3e02dfb4750058822f8b75054
murata0531 <coless115@gmail.com>
2021-07-13 09:29:45 +0900

    sbrk

--- apps/newlib_support.c
+++ apps/newlib_support.c
@@ -35,15 +35,37 @@ off_t lseek(int fd, off_t offset, int whence) {
   return -1;
 }
 
-ssize_t read(int fd, void* buf, size_t count) {
-  errno = EBADF;
+// #@@range_begin(newlib_open)
+int open(const char* path, int flags) {
+  struct SyscallResult res = SyscallOpenFile(path, flags);
+  if (res.error == 0) {
+    return res.value;
+  }
+  errno = res.error;
   return -1;
 }
+// #@@range_end(newlib_open)
 
-caddr_t sbrk(int incr) {
-  errno = ENOMEM;
-  return (caddr_t)-1;
+// #@@range_begin(newlib_read)
+ssize_t read(int fd, void* buf, size_t count) {
+  struct SyscallResult res = SyscallReadFile(fd, buf, count);
+  if (res.error == 0) {
+    return res.value;
+  }
+  errno = res.error;
+  return -1;
 }
+// #@@range_end(newlib_read)
+
+// #@@range_begin(newlib_sbrk)
+caddr_t sbrk(int incr) {
+  static uint8_t heap[4096];
+  static int i = 0;
+  int prev = i;
+  i += incr;
+  return (caddr_t)&heap[prev];
+}
+// #@@range_end(newlib_sbrk)
 
 ssize_t write(int fd, const void* buf, size_t count) {
   struct SyscallResult res = SyscallPutString(fd, buf, count);
