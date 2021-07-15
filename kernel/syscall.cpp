46e5efe53de88f376b9626af5662054a764bf598
murata0531 <coless115@gmail.com>
2021-07-15 14:15:32 +0900

    0_createが指定されたらファイルを新規に作成する

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -313,9 +313,20 @@ namespace {
     task.Files().emplace_back();
     return num_files;
   }
+
+  // #@@range_begin(create_file)
+  std::pair<fat::DirectoryEntry*, int> CreateFile(const char* path) {
+    auto [ file, err ] = fat::CreateFile(path);
+    switch (err.Cause()) {
+    case Error::kIsDirectory: return { file, EISDIR };
+    case Error::kNoSuchEntry: return { file, ENOENT };
+    case Error::kNoEnoughMemory: return { file, ENOSPC };
+    default: return { file, 0 };
+    }
+  }
+  // #@@range_end(create_file)
 } // namespace
 
-// #@@range_begin(open_file)
 SYSCALL(OpenFile) {
   const char* path = reinterpret_cast<const char*>(arg1);
   const int flags = arg2;
@@ -323,25 +334,29 @@ SYSCALL(OpenFile) {
   auto& task = task_manager->CurrentTask();
   __asm__("sti");
 
+  // #@@range_begin(open_file)
   if (strcmp(path, "@stdin") == 0) {
     return { 0, 0 };
   }
-// #@@range_end(open_file)
 
-  if ((flags & O_ACCMODE) == O_WRONLY) {
-    return { 0, EINVAL };
-  }
-
-  auto [ dir, post_slash ] = fat::FindFile(path);
-  if (dir == nullptr) {
-    return { 0, ENOENT };
-  } else if (dir->attr != fat::Attribute::kDirectory && post_slash) {
+  auto [ file, post_slash ] = fat::FindFile(path);
+  if (file == nullptr) {
+    if ((flags & O_CREAT) == 0) {
+      return { 0, ENOENT };
+    }
+    auto [ new_file, err ] = CreateFile(path);
+    if (err) {
+      return { 0, err };
+    }
+    file = new_file;
+  } else if (file->attr != fat::Attribute::kDirectory && post_slash) {
     return { 0, ENOENT };
   }
 
   size_t fd = AllocateFD(task);
-  task.Files()[fd] = std::make_unique<fat::FileDescriptor>(*dir);
+  task.Files()[fd] = std::make_unique<fat::FileDescriptor>(*file);
   return { fd, 0 };
+  // #@@range_end(open_file)
 }
 
 SYSCALL(ReadFile) {
