feb05e6e04c402167f68b36c1c520fd2c3bf152b
murata0531 <coless115@gmail.com>
2021-07-19 05:39:49 +0900

    MapFileシステムコールを呼び出す

--- apps/syscall.h
+++ apps/syscall.h
@@ -41,9 +41,8 @@ struct SyscallResult SyscallCreateTimer(
 
 struct SyscallResult SyscallOpenFile(const char* path, int flags);
 struct SyscallResult SyscallReadFile(int fd, void* buf, size_t count);
-// #@@range_begin(demand_pages_decl)
 struct SyscallResult SyscallDemandPages(size_t num_pages, int flags);
-// #@@range_end(demand_pages_decl)
+struct SyscallResult SyscallMapFile(int fd, size_t* file_size, int flags);
 
 #ifdef __cplusplus
 } // extern "C"
